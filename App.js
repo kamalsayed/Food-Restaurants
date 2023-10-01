2e718531c71fc98ace318fb6778f5472acb00183
kamalsayed <44102251+kamalsayed@users.noreply.github.com>
2023-10-02 01:51:27 +0300

    Update App.js

--- App.js
+++ App.js
@@ -31,15 +31,3 @@ export default createAppContainer(navigator);
 
 
 
-/*
-Yelp API
-
-Client ID
-
-JvhxnWjlcC9EDtLUpoy8JA
-API Key
-
-pJYqRzDl1j_7hkjdOxJK7DSAnzw7qWxG8PtRGuEhQ-NsBDx0GNYG1xEk8XHTYgzLhU_L2ilijcq2ObGBG8uTRnHaIhwQZV8ajj4Xhf-5Zj2N-0MdKGewpTC8U06wY3Yx
-
-
-*/
